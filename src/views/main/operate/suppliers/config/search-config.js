cfdb0d5f8aa3812bc47c696730da730d9e390999
huanggaofeng <huanggaofeng@linewell.com>
2022-12-25 16:01:29 +0800

    完成供应商列表查询功能

---
+++ src/views/main/operate/suppliers/config/search-config.js
@@ -0,0 +1,17 @@
+export const searchFormConfig = {
+  labelWidth: '120px',
+  headerTitle: '供应商查询',
+  itemStyle: {
+    padding: '10px 40px'
+  },
+  formItems: [
+    {
+      field: 'brand',
+      type: 'input',
+      label: '供应商名称',
+      rules: [],
+      placeholder: '请输入名称',
+      options: []
+    }
+  ]
+}
--- src/views/main/operate/suppliers/suppliers.vue
+++ src/views/main/operate/suppliers/suppliers.vue
@@ -1,34 +1,55 @@
 <template>
   <div class="suppliers">
+    <!-- 搜索框 -->
+    <page-search
+      :searchFormConfig="searchConfig"
+      @queryBtnClick="handleQueryClick"
+    />
     <!-- 列表 -->
     <page-content
       ref="pageContentRef"
       pageName="suppliers"
       :contentTableConfig="contentConfig"
+      :queryInfo="query"
       class="page-content"
     />
   </div>
 </template>
 
 <script>
+import pageSearch from '@/components/page-search/page-search.vue'
 import PageContent from '@/components/page-content/page-content.vue'
+
+import { searchFormConfig } from './config/search-config'
 import { contentTableConfig } from './config/content-config'
 
 export default {
   name: 'Suppliers',
   components: {
+    pageSearch,
     PageContent
   },
+  data() {
+    return {
+      query: null
+    }
+  },
   computed: {
+    searchConfig() {
+      return searchFormConfig
+    },
     contentConfig() {
       return contentTableConfig
+    }
+  },
+  methods: {
+    handleQueryClick(queryInfo) {
+      console.log(queryInfo)
+      this.query = queryInfo
+      this.$refs.pageContentRef.getPageData(queryInfo)
     }
   }
 }
 </script>
 
-<style lang="less" scoped>
-.page-content {
-  border-top: 0;
-}
-</style>
+<style lang="less" scoped></style>
