6c0334dac7a3503b90ae3cc8f329eb72ca03fd95
huanggaofeng <huanggaofeng@linewell.com>
2022-12-25 15:21:47 +0800

    角色列表细节调节

--- src/views/main/system/role/config/content-config.js
+++ src/views/main/system/role/config/content-config.js
@@ -1,6 +1,6 @@
 export const contentTableConfig = {
   title: '角色列表',
-  handleTitle: '新建角色',
+  handleTitle: '角色',
   // 自定义列表属性列参数
   propList: [
     { prop: 'id', label: 'ID', minWidth: '100' },
@@ -16,12 +16,12 @@ export const contentTableConfig = {
       label: '更新时间',
       minWidth: '250',
       slotName: 'updateAt'
-    },
-    {
-      label: '操作',
-      minWidth: '130',
-      slotName: 'handler'
     }
+    // {
+    //   label: '操作',
+    //   minWidth: '130',
+    //   slotName: 'handler'
+    // }
   ],
   showIndexColumn: false,
   showSelectColumn: true
